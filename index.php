f5b38dc254bbf75807af8b5b7dc746966a4b6c3b
TomaszKurowski <admin@stocktok.app>
2022-09-12 11:52:14 +0200

    Meta description

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ include('config.php'); ?>
     <meta http-equiv="X-UA-Compatible"           content="ie=edge" />
     <meta name="viewport"                        content="width=device-width, initial-scale=1.0" />
     <meta name="theme-color"                     content="<?= $config['theme_color'] ?>" />
-    <meta name="description"                     content="Be a market player" /> 
+    <meta name="description"                     content="Find stocks, currencies, crypto, fantokens, commodities, indices and more" /> 
     <meta name="mobile-web-app-capable"          content="yes" />
     <meta name="mobile-web-app-status-bar-style" content="black" />
     <meta name="mobile-web-app-title"            content="Stocktok" />    
